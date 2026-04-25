a38c1ec2eeab35982b57fe2c9054931c94aacfa1
lucas <lucasroberto7258@gmail.com>
2026-04-24 22:40:27 -0300

    IMC

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double tamanhobytes, velocidadembps, tempomin;
+
+Console.WriteLine("Tempo de Download\n");
+
+Console.Write("Tamanho do arquivo em MB: ");
+tamanhobytes = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Velocidade da conexão em Mbps...: ");
+velocidadembps = Convert.ToDouble(Console.ReadLine());
+
+tempomin = tamanhobytes * 8 / velocidadembps / 60;
+
+Console.WriteLine($"\nO tempo estimado de download: {tempomin:N1} minutos");
